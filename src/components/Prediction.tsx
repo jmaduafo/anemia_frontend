408e6c7416010d3a56e0015dd1347a29fa81e1b2
Jasmine Maduafokwa <jmaduafokwa@hotmail.com>
2025-03-03 13:52:10 +0100

    added backend url to post request in frontend

--- src/components/Prediction.tsx
+++ src/components/Prediction.tsx
@@ -155,7 +155,7 @@ function Prediction() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://anemia-backend-gmmx.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
